2b18bcfb84cb0a9a2a099ac733e717abaebe5d2c
imaneikb <65551499+imaneikb@users.noreply.github.com>
2020-11-04 15:00:02 +0100

    Update Main.java

--- Composite/src/Main.java
+++ Composite/src/Main.java
@@ -9,8 +9,8 @@ public class Main {
 		Fichier child2 = new Fichier(100);
 
 		Repertoire rep= new Repertoire(50);
-		rep.addFile(child1);
-		rep.addFile(child2);
+		rep.add(child1);
+		rep.add(child2);
 
 		System.out.println(child1.getTaille());      // output : 50
 		System.out.println(root.Calculer());    // output : 150
