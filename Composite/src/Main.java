a49d02c6443ef81309d7371f5744635fc9931465
imaneikb <65551499+imaneikb@users.noreply.github.com>
2020-11-04 14:59:26 +0100

    Update Main.java

--- Composite/src/Main.java
+++ Composite/src/Main.java
@@ -5,15 +5,16 @@ public class Main {
      public static void main(String[] args)
      {
 		
-		Fichier monfichier = new Fichier() ;
-		System.out.println("Le nom de mon fichier est:");
-		System.out.println(monfichier.getNom());
-		System.out.println("size");
+		Fichier child1 = new Fichier(50);
+		Fichier child2 = new Fichier(100);
 
+		Repertoire rep= new Repertoire(50);
+		rep.addFile(child1);
+		rep.addFile(child2);
 
-		Repertoire repertoire = new Repertoire() ;
-		repertoire.add(monfichier);
-		repertoire.remove(monfichier);
+		System.out.println(child1.getTaille());      // output : 50
+		System.out.println(root.Calculer());    // output : 150
+	}
 		
 	 }
 }
