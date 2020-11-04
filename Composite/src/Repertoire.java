7f68ca125b9a8ffbf7bee0f8ebeead01eece479f
imaneikb <65551499+imaneikb@users.noreply.github.com>
2020-11-04 14:19:12 +0100

    Update Repertoire.java

--- Composite/src/Repertoire.java
+++ Composite/src/Repertoire.java
@@ -3,8 +3,9 @@ import java.util.Collection;
 
 public abstract class Repertoire implements Composant {
 	private Collection children;
-	private int taille;
+	private List <Composant> listElement = new ArrayList<>();
 	private String nom;
+	private int taille;
 	
 
     public void RepertoireComposite() {
@@ -21,5 +22,14 @@ public abstract class Repertoire implements Composant {
     public void remove(Composant composant){
         children.remove(composant);
     }
+    
+    public int calculer(){
+	    int taille=0;
+	    for(Composant composant:listElement){
+		    taille=taille+composent.getTaille();
+	    }
+	    return taille;
+    }
+		    
 
 }
